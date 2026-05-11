45438c60878b0855f58ba9abf592de6a3d68a919
Teemo0104 <wh010405@qq.com>
2026-05-12 07:26:08 +0800

    feat: 16-gun configurable recoil + JT/big-jump + B3 trigger; flash persist RAM-safe

--- firmware/src/main.cc
+++ firmware/src/main.cc
@@ -11,6 +11,7 @@
 #endif
 #include <hardware/flash.h>
 #include <hardware/gpio.h>
+#include <hardware/sync.h>
 #include <pico/bootrom.h>
 #include <pico/mutex.h>
 #include <pico/platform.h>
@@ -189,14 +190,10 @@ bool read_adc() {
 #endif
 
 void do_persist_config(uint8_t* buffer) {
-#if !PICO_COPY_TO_RAM
     uint32_t ints = save_and_disable_interrupts();
-#endif
     flash_range_erase(CONFIG_OFFSET_IN_FLASH, PERSISTED_CONFIG_SIZE);
     flash_range_program(CONFIG_OFFSET_IN_FLASH, buffer, PERSISTED_CONFIG_SIZE);
-#if !PICO_COPY_TO_RAM
     restore_interrupts(ints);
-#endif
 }
 
 void reset_to_bootloader() {
